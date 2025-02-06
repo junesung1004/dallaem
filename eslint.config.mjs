41aa2b83a4c99422e9155e00c16d369e4b84a842
junesung1004 <junecoding1004@gmail.com>
2025-02-06 15:37:10 +0900

    fix: eslint 설정 중

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,69 +1,66 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-).map(config => ({
-    ...config,
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-})), {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+export default [
+	...compat
+		.extends(
+			'eslint:recommended',
+			'plugin:@typescript-eslint/recommended',
+			'plugin:react/recommended',
+		)
+		.map((config) => ({
+			...config,
+			files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+		})),
+	{
+		files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
 
-        ecmaVersion: 2021,
-        sourceType: "module",
+			ecmaVersion: 2021,
+			sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
-    },
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
 
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
-
-    // rules: {
-    //     "no-console": "error",
-    //     "react/react-in-jsx-scope": "off",
-    // },
-    "rules": {
-        "prettier/prettier": "warn",
-        "react/jsx-uses-react": "off",
-        // "react/react-in-jsx-scope": "off",
-        // "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
-            {
-                "args": "after-used",
-                "ignoreRestSiblings": true,
-                "varsIgnorePattern": "^_"
-            }
-        ],
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/unbound-method": "off",
-        "no-console": "error",
-        "react/react-in-jsx-scope": "off", // jsx 문법 관련 off
-    }
-}];
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		rules: {
+			// 'prettier/prettier': 'warn',
+			'no-console': 'error',
+			'react/jsx-uses-react': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'after-used',
+					ignoreRestSiblings: true,
+					varsIgnorePattern: '^_',
+				},
+			],
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/unbound-method': 'off',
+		},
+	},
+];
