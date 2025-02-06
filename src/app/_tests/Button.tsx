3d7f36be75a0b38422ed704f13d98ad237e61361
junesung1004 <junecoding1004@gmail.com>
2025-02-06 14:25:10 +0900

    fix: eslin 설정 오류

--- src/app/_tests/Button.tsx
+++ src/app/_tests/Button.tsx
@@ -1,15 +1,18 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import React from "react";
+import { useRouter } from 'next/navigation';
 
 export default function Button() {
-  const router = useRouter();
-  return (
-    <div className="flex justify-end pr-10">
-      <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={() => router.push("/createmodal")}>
-        모임 만들기!!!!!!!!!!!
-      </button>
-    </div>
-  );
+	const router = useRouter();
+	console.log('asdasdadsadd');
+	return (
+		<div className="flex justify-end pr-10">
+			<button
+				className="px-4 py-2 bg-blue-500 text-white rounded"
+				onClick={() => router.push('/createmodal')}
+			>
+				모임 만들기!!!!!!!!!!!
+			</button>
+		</div>
+	);
 }
