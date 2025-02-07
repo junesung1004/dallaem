eabbab9d4cdddcc1dd1a0f467c8490e309d175b2
avocad06 <miggo2704@gmail.com>
2025-02-07 13:36:35 +0900

    config: tailwind contWeight 속성 추가 commit lint type-enum 속성 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Pretendard", "sans-serif"],
-      },
-      fontSize: {
-        xs: ["12px", "16px"],
-        sm: ["14px", "20px"],
-        base: ["16px", "24px"],
-        lg: ["18px", "28px"],
-        xl: ["20px", "28px"],
-        "2xl": ["24px", "32px"],
-        "3xl": ["30px", "36px"],
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontWeight: {},
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Pretendard', 'sans-serif'],
+			},
+			fontSize: {
+				xs: ['12px', '16px'],
+				sm: ['14px', '20px'],
+				base: ['16px', '24px'],
+				lg: ['18px', '28px'],
+				xl: ['20px', '28px'],
+				'2xl': ['24px', '32px'],
+				'3xl': ['30px', '36px'],
+			},
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
