0b71305fe8498fac3ffb7b85310a2fc0108b66bd
sayiovme <jwluv123@gmail.com>
2025-02-07 10:36:18 +0900

    feat: merge test

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LoginPage() {
 	return (
 		<main className="min-h-screen md:w-[744px] lg:w-[996px] mx-auto">
-			로그인 페이지 - 수정 시작
+			로그인 페이지 - 수정 시작.
 		</main>
 	);
 }
