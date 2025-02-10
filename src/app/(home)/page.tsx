4e50bc519b4a2c573901bed58c41cc05a3824317
junesung1004 <junecoding1004@gmail.com>
2025-02-10 14:48:00 +0900

    style: main padding-right 수치 조정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import CardList from '../components/cardlist/CardList';
 
 export default async function Home() {
 	return (
-		<main className="container mx-auto px-5  w-full lg:w-[996px] ">
+		<main className="container mx-auto  pl-5 pr-8  w-full xl:w-[996px] ">
 			<p className="font-bold text-4xl pb-10">모임찾기 home page!!</p>
 			<Button />
 			<CardList />
