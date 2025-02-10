93993f7cd7ad04116e17198a6d73b1462cb3cfad
avocad06 <miggo2704@gmail.com>
2025-02-10 18:35:09 +0900

    chore: 공통 컴포넌트 시각화 적용

--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -4,9 +4,17 @@ import Badge from '@/app/components/Badge/Badge';
 function MyPage() {
 	return (
 		<div>
-			<div>
+			<div className="flex flex-col gap-1 p-4">
+				ProgressBar(20/40)
 				<ProgressBar max={40} value={20} isNeutral={true} isAnimate={false} />
-				<Badge content={1024} />
+				ProgressBar(20/140)
+				<ProgressBar max={140} value={20} isNeutral={false} isAnimate={true} />
+				Badge
+				<div className="flex gap-2">
+					<Badge content={1024} />
+					<Badge content={'0'} />
+					<Badge content={'19'} />
+				</div>
 			</div>
 		</div>
 	);
