d8281adeb459b47908a3cef9963ae43abd6c7693
avocad06 <miggo2704@gmail.com>
2025-02-10 18:24:26 +0900

    feat: 공통컴포넌트 Badge 추가

--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -1,10 +1,12 @@
 import ProgressBar from '@/app/components/ProgressBar/ProgressBar';
+import Badge from '@/app/components/Badge/Badge';
 
 function MyPage() {
 	return (
 		<div>
 			<div>
 				<ProgressBar max={40} value={20} isNeutral={true} isAnimate={false} />
+				<Badge content={1024} />
 			</div>
 		</div>
 	);
