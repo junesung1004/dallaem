3d5a5503dbf22121477e1f979c940cd9f19a3bda
junesung1004 <junecoding1004@gmail.com>
2025-02-11 16:41:14 +0900

    fix: vercel 에러 찾는 중

--- src/app/components/cardlist/CardList.tsx
+++ src/app/components/cardlist/CardList.tsx
@@ -25,36 +25,22 @@ export default function CardList() {
 		<div className='flex flex-col items-center gap-6'>
 			{dummy.map((el) => (
 				<Card key={el.id} isClear={el.isClear}>
-					<Card.ImageSection
-						// isClear={false}
-						src='/images/imgLogin.png'
-						alt='이미지 예시'
-					/>
+					<Card.ImageSection src='/images/imgLogin.png' alt='이미지 예시' />
 					<Card.Content>
 						<Card.Header
 							title='달램핏 오피스 스트레칭 |'
 							place='을지로 3가'
 							src='/images/save.png'
-							// isClear={false}
 							onClick={() => {
 								// console.log('클릭');
 							}}
 						>
-							<DateBadge
-								text='2025-02-11T00:29:52.866Z'
-								type='date'
-								// isClear={false}
-							/>
-							<DateBadge
-								text='2025-02-11T00:29:52.866Z'
-								type='time'
-								// isClear={false}
-							/>
+							<DateBadge text='2025-02-11T00:29:52.866Z' type='date' />
+							<DateBadge text='2025-02-11T00:29:52.866Z' type='time' />
 						</Card.Header>
 						<Card.Footer
 							max={40}
 							value={30}
-							// isClear={false}
 							status='개설확정'
 							onClick={() => {
 								// console.log('푸터 클릭');
