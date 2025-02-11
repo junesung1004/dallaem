d40fd9364d82800289649dc992f8239ee8b283e2
junesung1004 <junecoding1004@gmail.com>
2025-02-11 16:19:14 +0900

    fix: cardlist component 복사 파일 삭제

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,10 @@
 import Button from '@/app/_tests/Button';
-
 import CardList from '../components/CardList/CardList';
 
 export default async function Home() {
 	return (
 		<main className="container mx-auto px-5  w-full xl:w-[996px] ">
-			<p className="font-bold text-4xl pb-10">모임찾기 home page!!</p>
+			<p className="font-bold text-4xl pb-10">모임찾기 home page</p>
 			<Button />
 			<CardList />
 		</main>
