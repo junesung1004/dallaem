5f3b85936406c85d4c760010f58628cd3f478fae
junesung1004 <junecoding1004@gmail.com>
2025-02-11 16:29:28 +0900

    fix: vercel 오류

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,6 @@ import CardList from '../components/CardList/CardList';
 export default async function Home() {
 	return (
 		<main className='container mx-auto px-5  w-full xl:w-[996px] '>
-			<p className='font-bold text-4xl pb-10'>모임찾기 home page</p>
 			<Button />
 			<CardList />
 		</main>
