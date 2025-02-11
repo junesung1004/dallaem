cec0a7f37add6850347f9f5ee85810b3f0b54250
junesung1004 <junecoding1004@gmail.com>
2025-02-11 16:23:42 +0900

    fix: vecel error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,10 @@
 import Button from '@/app/_tests/Button';
-import CardList from '../components/CardList/CardList';
+import CardList from '@/app/components/CardList/CardList';
 
 export default async function Home() {
 	return (
-		<main className="container mx-auto px-5  w-full xl:w-[996px] ">
-			<p className="font-bold text-4xl pb-10">모임찾기 home page</p>
+		<main className='container mx-auto px-5  w-full xl:w-[996px] '>
+			<p className='font-bold text-4xl pb-10'>모임찾기 home page</p>
 			<Button />
 			<CardList />
 		</main>
