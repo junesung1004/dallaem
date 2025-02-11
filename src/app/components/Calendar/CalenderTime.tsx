b4e81e9a233ce06d369ed58eb01373149c4d14d8
junesung1004 <junecoding1004@gmail.com>
2025-02-11 22:14:01 +0900

    feat: react-datepicker 라이브러리 사용한 캘린더 작업

---
+++ src/app/components/Calendar/CalenderTime.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { setHours, setMinutes } from 'date-fns';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'; // 추가
+
+// react-datepicker 라이브러리 = 달력 + 시간 표기
+export const CalenderTime = () => {
+	const [startDate, setStartDate] = useState(
+		setHours(setMinutes(new Date(), 30), 16),
+	);
+
+	return (
+		<DatePicker
+			selected={startDate}
+			onChange={(date) => setStartDate(date)}
+			showTimeSelect
+			excludeTimes={[
+				setHours(setMinutes(new Date(), 0), 17),
+				setHours(setMinutes(new Date(), 30), 18),
+				setHours(setMinutes(new Date(), 30), 19),
+				setHours(setMinutes(new Date(), 30), 17),
+			]}
+			dateFormat='MMMM d, yyyy h:mm aa'
+			className='border border-gray-300 rounded-lg p-2'
+		/>
+	);
+};
