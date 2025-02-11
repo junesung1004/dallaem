6571c09fbe4c20e122bef641a3eb8cfdb2af9ad4
junesung1004 <junecoding1004@gmail.com>
2025-02-11 16:31:48 +0900

    fix: vercel 오류

--- src/app/components/cardlist/CardList.tsx
+++ src/app/components/cardlist/CardList.tsx
@@ -3,8 +3,13 @@
 import { DateBadge } from '../DateBadge';
 import Card from './Card';
 
+type Dummy = {
+	id: number;
+	isClear: boolean;
+};
+
 export default function CardList() {
-	const dummy = [
+	const dummy: Dummy[] = [
 		{ id: 1, isClear: true },
 		{ id: 2, isClear: true },
 		{ id: 3, isClear: false },
@@ -17,32 +22,32 @@ export default function CardList() {
 	];
 
 	return (
-		<div className="flex flex-col items-center gap-6">
+		<div className='flex flex-col items-center gap-6'>
 			{dummy.map((el) => (
 				<Card key={el.id} isClear={el.isClear}>
 					<Card.ImageSection
 						// isClear={false}
-						src="/images/imgLogin.png"
-						alt="이미지 예시"
+						src='/images/imgLogin.png'
+						alt='이미지 예시'
 					/>
 					<Card.Content>
 						<Card.Header
-							title="달램핏 오피스 스트레칭 |"
-							place="을지로 3가"
-							src="/images/save.png"
+							title='달램핏 오피스 스트레칭 |'
+							place='을지로 3가'
+							src='/images/save.png'
 							// isClear={false}
 							onClick={() => {
 								// console.log('클릭');
 							}}
 						>
 							<DateBadge
-								text="2025-02-11T00:29:52.866Z"
-								type="date"
+								text='2025-02-11T00:29:52.866Z'
+								type='date'
 								// isClear={false}
 							/>
 							<DateBadge
-								text="2025-02-11T00:29:52.866Z"
-								type="time"
+								text='2025-02-11T00:29:52.866Z'
+								type='time'
 								// isClear={false}
 							/>
 						</Card.Header>
@@ -50,7 +55,7 @@ export default function CardList() {
 							max={40}
 							value={30}
 							// isClear={false}
-							status="개설확정"
+							status='개설확정'
 							onClick={() => {
 								// console.log('푸터 클릭');
 							}}
