c17d9ddb8f09535659be979dedc6b04da8e9a5b8
sayiovme <jwluv123@gmail.com>
2025-02-12 09:04:50 +0900

    feat: 로그인 유효성 검사 기본 구현

--- src/app/api/userAuth.ts
+++ src/app/api/userAuth.ts
@@ -1,45 +1,34 @@
 import { signinUserInterface, signupUserInterface } from './userInterface';
 
 // 로그인 기능 : 토큰 반환
-const signinUser = async ({
-	email,
-	password,
-	onError = () => {},
-}: signinUserInterface) => {
-	try {
-		const response = await fetch(
-			`https://fe-adv-project-together-dallaem.vercel.app/7/auths/signin`,
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					email: email,
-					password: password,
-				}),
+const signinUser = async ({ email, password }: signinUserInterface) => {
+	const response = await fetch(
+		`https://fe-adv-project-together-dallaem.vercel.app/7/auths/signin`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
 			},
-		);
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		},
+	);
 
-		if (!response.ok) {
-			const error = await response.json();
-			throw new Error(error.message);
-		}
-
-		const data = await response.json();
-		const token = data.token;
-		// 토큰 만료 시간을 1시간으로 설정 (현재 시간 + 1시간)
-		const expiryTime = Date.now() + 60 * 60 * 1000;
-		// 토큰 저장
-		localStorage.setItem('authToken', token);
-		localStorage.setItem('tokenExpiryTime', expiryTime.toString()); // 만료 시간 저장
-		// 로그인 성공 시 에러 초기화
-		console.log(data);
-		onError('');
-	} catch (error: any) {
-		console.log('로그인 실패', error);
-		onError(error.message); // 에러 발생 시 부모 컴포넌트에 에러 메시지 전달
+	if (!response.ok) {
+		const error = await response.json();
+		throw new Error(error.message);
 	}
+
+	const data = await response.json();
+	const token = data.token;
+	// 토큰 만료 시간을 1시간으로 설정 (현재 시간 + 1시간)
+	const expiryTime = Date.now() + 60 * 60 * 1000;
+	// 토큰 저장
+	localStorage.setItem('authToken', token);
+	localStorage.setItem('tokenExpiryTime', expiryTime.toString()); // 만료 시간 저장
+	console.log(data);
 };
 
 // 회원가입 기능
