54cbc2d9210fa4b391c58d7f1b6938d86e50d90b
junesung1004 <junecoding1004@gmail.com>
2025-02-12 15:18:30 +0900

    fix: 로그인 페이지  코드 충돌

--- src/app/_tests/Count.tsx
+++ src/app/_tests/Count.tsx
@@ -7,7 +7,7 @@ export default function Count() {
 	const { count, increase, decrease } = useCount();
 	return (
 		<div className='pt-10 flex flex-col gap-5'>
-			<h1>Count :: {count}</h1>
+			<h1>Count : {count}</h1>
 			<button className='w-[150px] py-2 px-10 border-2' onClick={increase}>
 				+ 버튼
 			</button>
