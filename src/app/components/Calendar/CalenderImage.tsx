17f363039ce32b2e3918668ead00e2a987e5fbcd
junesung1004 <junecoding1004@gmail.com>
2025-02-17 13:33:34 +0900

    feat: 캘린더 인풋으로 나오는게 아닌 캘린더 이미지로 나오는 컴포넌트 추가

--- src/app/components/Calendar/CalenderImage.tsx
+++ src/app/components/Calendar/CalenderImage.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-// react-datepicker 라이브러리 = 달력만 표기
+// react-datepicker 라이브러리 = 달력만 표기~
 export const CalenderImage = () => {
 	const [startDate, setStartDate] = useState<Date | null>(new Date());
 
