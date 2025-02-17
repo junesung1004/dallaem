49663685e168362c91b286eda524921122146789
avocad06 <miggo2704@gmail.com>
2025-02-17 13:59:03 +0900

    feat: button 공통 컴포넌트 variation props 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,33 @@ export interface ButtonProps
 	isOutlined: boolean;
 	children: React.ReactNode;
 	isFull?: boolean;
+	variation?: 'default' | 'outline';
 }
 
-function Button({ children, state, isOutlined, isFull, ...rest }: ButtonProps) {
-	const buttonStateMap = {
+function Button({
+	children,
+	state,
+	variation = 'default',
+	isOutlined,
+	isFull,
+	...rest
+}: ButtonProps) {
+	const buttonsStateMap = {
+		default: {
+			initial: 'bg-orange-600 text-white',
+			hover: 'hover:bg-orange-700 hover:text-white',
+			active: 'active:bg-orange-800 active:text-white',
+			disabled: 'bg-gray-400 text-white',
+		},
+		outline: {
+			initial: 'bg-orange-600 text-white',
+			hover: 'hover:bg-orange-700 hover:text-white',
+			active: 'active:bg-orange-800 active:text-white',
+			disabled: 'bg-white text-gray-400 border border-gray-400',
+		},
+	};
+
+	const buttonStateMap1 = {
 		default: {
 			initial: 'bg-orange-600 text-white',
 			outlined: 'bg-white text-orange-600 border border-orange-600',
@@ -26,22 +49,16 @@ function Button({ children, state, isOutlined, isFull, ...rest }: ButtonProps) {
 		},
 	};
 
-	const hoverProp = isOutlined
-		? `hover:bg-white hover:text-orange-500 hover:border hober:border-orange-500`
-		: `hover:bg-orange-700 hover:text-white`;
-
-	const activeProp = isOutlined
-		? `active:bg-white active:text-orange-700 active:border active:border-orange-700`
-		: `active:bg-orange-800 active:text-white`;
+	// 기본 버튼 스타일
+	const { initial, hover, active, disabled } = buttonsStateMap[variation];
+	const buttonStyle = `${initial} ${hover} ${active} ${rest.disabled ? disabled : ''}`;
 
 	return (
 		<button
 			className={`
                 inline-flex px-[3rem] py-2 rounded-xl justify-center item-center
-                ${buttonStateMap[state][isOutlined ? 'outlined' : 'initial']} 
-                ${isFull ? 'btn-full' : 'btn-default'} 
-								${hoverProp}
-								${activeProp}
+                ${isFull ? 'btn-full' : 'btn-default'}
+								${buttonStyle}
                 `}
 			{...rest}
 		>
