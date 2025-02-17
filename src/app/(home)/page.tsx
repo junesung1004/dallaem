6fd0f4f34a46f14b3b707a3b11c5eae3ca38a54a
avocad06 <miggo2704@gmail.com>
2025-02-17 11:21:21 +0900

    style: 공통 레이아웃 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import PageNavbar from '@/components/PageNav/PageNavbar';
 
 export default async function Home() {
 	return (
-		<div className='mx-auto px-5 sm:px-16 xl:px-0 py-10  w-full xl:w-[996px] '>
+		<div className=''>
 			{/* 함께 할 사람이 없나요? */}
 			<PageInfo pageKey='meetings' />
 
