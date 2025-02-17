bd1a0ae082df3d39e4f4da97acaaef0377714feb
avocad06 <miggo2704@gmail.com>
2025-02-18 04:17:19 +0900

    config: next js 원격 이미지 허용 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ module.exports = {
 				port: '',
 				pathname: '/*',
 			},
+			{
+				protocol: 'https',
+				hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+				port: '',
+				pathname: '/**',
+			},
 		],
 	},
 };
