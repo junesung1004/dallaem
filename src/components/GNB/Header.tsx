52a6e974ef5e1d557f5a268e4f662dbb4a29c3f7
avocad06 <miggo2704@gmail.com>
2025-02-18 10:21:13 +0900

    style: 로그인, 회원가입 페이지 공통 레이아웃 적용

--- src/components/GNB/Header.tsx
+++ src/components/GNB/Header.tsx
@@ -30,36 +30,38 @@ export default function Header() {
 	}, [pathname]);
 
 	return (
-		<header className='flex flex-col justify-center w-full h-[60px] mx-auto p-2 pr-5  bg-orange-600 border-black border-b-2'>
-			<nav className='md:mx-auto w-full  gap-4 lg:w-[996px] xl:w-[1198px] flex justify-between text-xs md:text-base lg:text-lg text-white'>
-				<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
-					<li className='font-bold'>
-						<Link href={'/'}>같이 달램</Link>
-					</li>
-					<li>
-						<Link href={'/'}>모임 찾기</Link>
-					</li>
-					<li>
-						<Link href={'/favorite-meetings'}>찜한 모임</Link>
-					</li>
-					<li>
-						<Link href={'/all-reviews'}>모든 리뷰</Link>
-					</li>
-				</ul>
-
-				<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
-					<li>
-						<Link href={isLoggedIn ? '/mypage' : '/login'}>
-							{isLoggedIn ? '마이페이지' : '로그인'}
-						</Link>
-					</li>
-					{isLoggedIn && (
-						<li>
-							<button onClick={logoutUser}>로그아웃</button>
+		<header className='flex flex-col justify-center w-full h-[56px] md:h-[60px] mx-auto bg-orange-600 border-black border-b-2 px-4 md:px-6 lg:px-[106px] py-10'>
+			<div className='max-w-[1200px] w-full mx-auto'>
+				<nav className='md:mx-auto w-full gap-4 lg:w-[996px] xl:w-[1198px] flex justify-between text-xs md:text-base lg:text-lg text-white'>
+					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
+						<li className='font-bold'>
+							<Link href={'/'}>같이 달램</Link>
 						</li>
-					)}
-				</ul>
-			</nav>
+						<li>
+							<Link href={'/'}>모임 찾기</Link>
+						</li>
+						<li>
+							<Link href={'/favorite-meetings'}>찜한 모임</Link>
+						</li>
+						<li>
+							<Link href={'/all-reviews'}>모든 리뷰</Link>
+						</li>
+					</ul>
+
+					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
+						<li>
+							<Link href={isLoggedIn ? '/mypage' : '/login'}>
+								{isLoggedIn ? '마이페이지' : '로그인'}
+							</Link>
+						</li>
+						{isLoggedIn && (
+							<li>
+								<button onClick={logoutUser}>로그아웃</button>
+							</li>
+						)}
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 }
