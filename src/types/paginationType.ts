47bf64994ca9aee788826be8098b75afcbbaa7b4
심우석 <dntjr5741@naver.com>
2025-02-18 13:36:31 +0900

    feat: datailpage API 연결

--- src/types/paginationType.ts
+++ src/types/paginationType.ts
@@ -1,22 +1,3 @@
-export interface DummyDataType {
-	teamId: string;
-	id: number;
-	type: string;
-	name: string;
-	dateTime: string;
-	registrationEnd: string;
-	location: string;
-	participantCount: number;
-	capacity: number;
-	image: string;
-	createdBy: number;
-	canceledAt: string | null;
-}
-
-export interface PaginationProps {
-	data: DummyDataType[];
-}
-
 export interface ReviewType {
 	teamId: string;
 	id: number;
