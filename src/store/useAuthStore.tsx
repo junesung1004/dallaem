3dda11561cbb23ef334e92b3b17d53e3eeef88b8
sayiovme <jwluv123@gmail.com>
2025-02-19 18:11:35 +0900

    feat: 유저 상태관리 types 추가

--- src/store/useAuthStore.tsx
+++ src/store/useAuthStore.tsx
@@ -4,6 +4,10 @@ interface Store {
 	isLoggedIn: boolean;
 	token: string | null;
 	userId: number | null;
+	email: string | null;
+	name: string | null;
+	companyName: string | null;
+	image: string | null;
 	setIsLoggedIn: (status: boolean) => void;
 	setToken: (newToken: string | null) => void;
 	setUserId: (id: number | null) => void;
@@ -14,14 +18,27 @@ const useStore = create<Store>((set) => ({
 	isLoggedIn: false,
 	token: null,
 	userId: null,
+	email: null,
+	name: null,
+	companyName: null,
+	image: null,
 	setIsLoggedIn: (status: boolean) => set({ isLoggedIn: status }),
 	setToken: (newToken: string | null) => set({ token: newToken }),
 	setUserId: (id: number | null) => set({ userId: id }),
+	setEmail: (email: string | null) => set({ email: email }),
+	setName: (name: string | null) => set({ name: name }),
+	setCompanyName: (companyName: string | null) =>
+		set({ companyName: companyName }),
+	setImage: (image: string | null) => set({ image: image }),
 	setUserNull: () =>
 		set({
 			isLoggedIn: false,
 			token: null,
 			userId: null,
+			email: null,
+			name: null,
+			companyName: null,
+			image: null,
 		}),
 }));
 
