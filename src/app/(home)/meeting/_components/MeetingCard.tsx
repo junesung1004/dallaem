0d88eddc9b28f93efbb51d7b08ae8c0dbe501f5a
심우석 <dntjr5741@naver.com>
2025-02-21 17:20:46 +0900

    feat: 카드 디테일 컴포넌트 조합

--- src/app/(home)/meeting/_components/MeetingCard.tsx
+++ src/app/(home)/meeting/_components/MeetingCard.tsx
@@ -5,10 +5,18 @@ import { DateBadge } from '../../../../components/Badge/DateBadge';
 import { MeetingCardProps } from '../../../../types/meetingDetail';
 import { LikeButton } from '../../../../components/Button/LikeButton';
 import { AttendeeProfiles } from './AttendeeProfiles';
+import { StatusBadge } from '@/components/Badge/StatusBadge';
+import ProgressBar from '@/components/ProgressBar/ProgressBar';
 
-export const MeetingCard = ({ location, type, date, id }: MeetingCardProps) => {
+export const MeetingCard = ({
+	location,
+	type,
+	date,
+	id,
+	capacity,
+	participantCount,
+}: MeetingCardProps) => {
 	const [userId, setUserId] = useState<number | undefined>(undefined);
-
 	useEffect(() => {
 		const storedUserId = localStorage.getItem('userId');
 		if (storedUserId) {
@@ -26,21 +34,32 @@ export const MeetingCard = ({ location, type, date, id }: MeetingCardProps) => {
 					<div className='text-lg font-bold'>{type}</div>
 					<div className='text-sm text-gray-600'>{location}</div>
 
-					<div className='flex items-center gap-2 my-2 mb-7'>
+					<div className='flex items-center gap-2 my-2 mb-5'>
 						<DateBadge text={date} type='date' />
 						<DateBadge text={date} type='time' />
 					</div>
 				</div>
 			</div>
+			<div className='w-full border-b-2  border-dashed border-gray-200'></div>
 
-			<div className='border-[1.2px] border-gray-200 border-dashed mb-2 px-[24px]'>
-				<div id='AttendeeProfiles'>
-					<AttendeeProfiles gatheringId={id} />
+			<div className='px-7 py-3 space-y-3  mb-2'>
+				<div className='flex justify-between mt-3'>
+					<div className='flex space-x-5'>
+						<div className='font-bold'>모집 정원 {capacity}명</div>
+						<div id='AttendeeProfiles'>
+							<AttendeeProfiles gatheringId={id} />
+						</div>
+					</div>
+					<div>
+						<StatusBadge />
+					</div>
+				</div>
+				<ProgressBar value={participantCount} max={capacity} isAnimate={true} />
+				<div className='flex justify-between'>
+					<div>최소인원 5명</div>
+					<div className='text-orange-500'>최대인원 20명</div>
 				</div>
 			</div>
-
-			{/* 여기 아래는 더미데이터 */}
-			<div className='mb-28'></div>
 		</div>
 	);
 };
