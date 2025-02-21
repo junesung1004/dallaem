8b07794b6d577881b8ac6709ecb068172860d83a
sayiovme <jwluv123@gmail.com>
2025-02-21 11:29:57 +0900

    feat: 헤더 프로필 툴팁 추가

--- src/components/GNB/Header.tsx
+++ src/components/GNB/Header.tsx
@@ -4,14 +4,15 @@ import { useAuthStore } from '@/store/useAuthStore';
 import { useAuth } from '@/hooks/customs/useAuth';
 import Link from 'next/link';
 import React from 'react';
+import { ProfileTooltip } from '@/components/GNB/profileTooltip';
 
 export default function Header() {
 	const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-	const { logoutUser } = useAuth();
+
 	return (
 		<header className='flex flex-col justify-center w-full h-[56px] md:h-[60px] mx-auto bg-orange-600 border-black border-b-2 px-4 md:px-6 lg:px-[106px]'>
 			<div className='max-w-[1200px] w-full mx-auto'>
-				<nav className='md:mx-auto w-full gap-4 lg:w-[996px] xl:w-[1198px] flex justify-between text-xs md:text-base lg:text-lg text-white'>
+				<nav className='md:mx-auto w-full flex justify-between items-center text-xs md:text-base lg:text-lg text-white'>
 					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
 						<li className='font-bold'>
 							<Link href={'/'}>같이 달램</Link>
@@ -29,15 +30,12 @@ export default function Header() {
 
 					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
 						<li>
-							<Link href={isLoggedIn ? '/mypage' : '/login'}>
-								{isLoggedIn ? '마이페이지' : '로그인'}
-							</Link>
+							{isLoggedIn ? (
+								<ProfileTooltip />
+							) : (
+								<Link href='/login'>로그인</Link>
+							)}
 						</li>
-						{isLoggedIn && (
-							<li>
-								<button onClick={logoutUser}>로그아웃</button>
-							</li>
-						)}
 					</ul>
 				</nav>
 			</div>
