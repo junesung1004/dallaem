a9a4d57e4b16ee8b0fac39bea10cdad0543ba37f
avocad06 <miggo2704@gmail.com>
2025-02-21 10:38:10 +0900

    feat: 찜한 모임 페이지 커밋 변경

--- src/api/meeting/getMeetingDate.ts
+++ src/api/meeting/getMeetingDate.ts
@@ -38,3 +38,27 @@ export const getMeetingData = async ({
 		return null;
 	}
 };
+
+/** 찜한 페이지 data 가져오기 */
+export const getFavoriteMeetingData = async (filters) => {
+	const queryParams = new URLSearchParams(filters);
+	try {
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_BASE_URL}/gatherings?${queryParams.toString()}`,
+		);
+
+		if (!res.ok) {
+			throw new Error(`서버 오류: ${res.status} ${res.statusText}`);
+		}
+
+		const data = await res.json();
+
+		const filteredData = data.filter(
+			(item: { image: string | null }) => item.image !== null,
+		);
+		return filteredData;
+	} catch (error) {
+		console.error('미팅 목록 가져오기 실패:', error);
+		return null;
+	}
+};
