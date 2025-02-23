157d231ff1022a689318e31744a154805f501afb
Chae <pce2416@gmail.com>
2025-02-23 19:22:18 +0900

    feat: 모든 리뷰 관련 api 추가

--- src/app/(home)/all-reviews/page.tsx
+++ src/app/(home)/all-reviews/page.tsx
@@ -1,33 +1,16 @@
 'use client';
 
 import FilterList from '@/components/Filtering/FIlterList';
-import { ReviewScore } from '@/types/reviewType';
-import { useEffect, useState } from 'react';
-import ReviewScores from './_components/reviewScores';
 import PageNavbar from '@/components/PageNav/PageNavbar';
 import PageInfo from '@/components/PageInfo/PageInfo';
-import ReviewCard from '@/components/ReviewCard/ReviewCard';
-import { getReviewScore } from '@/api/getReveiwScore';
 import { useFetchReviews } from '@/hooks/customs/useFetchReviews';
-import { useFilterStore } from '@/store/useInputSelectFilterStore';
+import ReviewScores from './_components/ReviewScores';
+import ReviewCardList from './_components/ReviewCardList';
+import useFetchReviewScores from '@/hooks/query/useFetchReviewsScore';
 
 export default function AllReviews() {
-	const { selectedFilters } = useFilterStore();
-	const [reviewScore, setReviewScore] = useState<ReviewScore[] | null>(null);
-
-	useEffect(() => {
-		console.log('현재 필터 값:', selectedFilters);
-	}, [selectedFilters]);
-
-	useFetchReviews();
-
-	useEffect(() => {
-		getReviewScore({ gatheringId: '', type: '' })
-			.then((res) => {
-				setReviewScore(res);
-			})
-			.catch((err) => console.log(err));
-	}, []);
+	const reviews = useFetchReviews();
+	const reviewsScore = useFetchReviewScores({ gatheringId: '' });
 
 	return (
 		<div className='flex flex-col gap-6'>
@@ -35,44 +18,10 @@ export default function AllReviews() {
 				<PageInfo pageKey='reviews' />
 				<PageNavbar pageKey='meetings' />
 			</div>
-			{reviewScore && reviewScore.length > 0 && (
-				<ReviewScores reviewScore={reviewScore[0]} />
-			)}
+			{reviewsScore && <ReviewScores reviewScore={reviewsScore} />}
 			<div className='flex flex-col gap-4 p-4 bg-white border-t-2 border-gray-900'>
-				<FilterList
-					// 사용 가능한 필터 선택
-					enabledFilters={['location', 'date', 'sortByReview']}
-				/>
-				<div>
-					<ReviewCard>
-						<ReviewCard.ImageSection src='/images/imgLogin.png' />
-						<ReviewCard.ReviewLayout>
-							<ReviewCard.HeartScore score={5} />
-							<ReviewCard.Content comment='따듯하게 느껴지는 공간이에요 :) 평소에 달램 이용해보고 싶었는데 이렇게 같이 달램 생기니까 너무 좋아요! 프로그램이 더 많이 늘어났으면 좋겠어요.' />
-							<ReviewCard.EtcInfo
-								userIcon='/images/profile/profileDefaultLarge.png'
-								nickname='박채'
-								type='DALLAEMFIT'
-								location='건대입구'
-								date='2025-02-12'
-							/>
-						</ReviewCard.ReviewLayout>
-					</ReviewCard>
-					<ReviewCard>
-						<ReviewCard.ImageSection src='/images/imgLogin.png' />
-						<ReviewCard.ReviewLayout>
-							<ReviewCard.HeartScore score={5} />
-							<ReviewCard.Content comment='좋아요요' />
-							<ReviewCard.EtcInfo
-								userIcon='/images/profile/profileDefaultLarge.png'
-								nickname='박채'
-								type='DALLAEMFIT'
-								location='건대입구'
-								date='2025-02-12'
-							/>
-						</ReviewCard.ReviewLayout>
-					</ReviewCard>
-				</div>
+				<FilterList enabledFilters={['location', 'date', 'sortByReview']} />
+				<ReviewCardList reviews={reviews} />
 			</div>
 		</div>
 	);
