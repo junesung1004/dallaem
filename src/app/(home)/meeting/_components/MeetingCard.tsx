90f521944e1748d7c0e021ae12c8256d51506efd
sayiovme <jwluv123@gmail.com>
2025-02-24 14:13:21 +0900

    feat: GNB Indicator Animation, font-weight 수정

--- src/app/(home)/meeting/_components/MeetingCard.tsx
+++ src/app/(home)/meeting/_components/MeetingCard.tsx
@@ -5,18 +5,10 @@ import { DateBadge } from '../../../../components/Badge/DateBadge';
 import { MeetingCardProps } from '../../../../types/meetingDetail';
 import { LikeButton } from '../../../../components/Button/LikeButton';
 import { AttendeeProfiles } from './AttendeeProfiles';
-import { StatusBadge } from '@/components/Badge/StatusBadge';
-import ProgressBar from '@/components/ProgressBar/ProgressBar';
 
-export const MeetingCard = ({
-	location,
-	type,
-	date,
-	id,
-	capacity,
-	participantCount,
-}: MeetingCardProps) => {
+export const MeetingCard = ({ location, type, date, id }: MeetingCardProps) => {
 	const [userId, setUserId] = useState<number | undefined>(undefined);
+
 	useEffect(() => {
 		const storedUserId = localStorage.getItem('userId');
 		if (storedUserId) {
@@ -34,32 +26,21 @@ export const MeetingCard = ({
 					<div className='text-lg font-bold'>{type}</div>
 					<div className='text-sm text-gray-600'>{location}</div>
 
-					<div className='flex items-center gap-2 my-2 mb-5'>
+					<div className='flex items-center gap-2 my-2 mb-7'>
 						<DateBadge text={date} type='date' />
 						<DateBadge text={date} type='time' />
 					</div>
 				</div>
 			</div>
-			<div className='w-full border-b-2  border-dashed border-gray-200'></div>
 
-			<div className='px-7 py-3 space-y-3  mb-2'>
-				<div className='flex justify-between mt-3'>
-					<div className='flex space-x-5'>
-						<div className='font-bold'>모집 정원 {capacity}명</div>
-						<div id='AttendeeProfiles'>
-							<AttendeeProfiles gatheringId={id} />
-						</div>
-					</div>
-					<div>
-						<StatusBadge />
-					</div>
-				</div>
-				<ProgressBar value={participantCount} max={capacity} isAnimate={true} />
-				<div className='flex justify-between '>
-					<div>최소인원 5명</div>
-					<div className='text-orange-500'>최대인원 20명</div>
+			<div className='border-[1.2px] border-gray-200 border-dashed mb-2 px-[24px]'>
+				<div id='AttendeeProfiles'>
+					<AttendeeProfiles gatheringId={id} />
 				</div>
 			</div>
+
+			{/* 여기 아래는 더미데이터 */}
+			<div className='mb-28'></div>
 		</div>
 	);
 };
