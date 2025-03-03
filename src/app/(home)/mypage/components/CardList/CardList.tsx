aff1fd9506a00bf9c3a101995c3e280c4d43fcbe
avocad06 <miggo2704@gmail.com>
2025-03-03 09:41:44 +0900

    fix: 마이페이지 데이터 없을 때 UI 처리

--- src/app/(home)/mypage/components/CardList/CardList.tsx
+++ src/app/(home)/mypage/components/CardList/CardList.tsx
@@ -9,8 +9,24 @@ interface CardListProps {
 	pageKey: 'joined' | 'review' | 'hosted';
 }
 
+/** no data const */
+const noDataMsg = {
+	joined: '신청한 모임이 아직 없어요',
+	review: '아직 작성 가능한 리뷰가 없어요',
+	hosted: '아직 만든 모임이 없어요',
+};
+
 function CardList({ cardType, pageKey }: CardListProps) {
 	const { meetings, onCancelClick } = useMyMeetings(pageKey);
+
+	/** 데이터 없을 경우 처리 */
+	if (!meetings?.length) {
+		return (
+			<div className='flex justify-center items-center mx-auto my-auto'>
+				<span>{noDataMsg[pageKey ?? 'joined']}</span>
+			</div>
+		);
+	}
 
 	return (
 		<div>
--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -1,7 +1,11 @@
 import CardList from './components/CardList/CardList';
 
 function MyPage() {
-	return <CardList cardType='joined' pageKey='joined' />;
+	return (
+		<div className='flex min-h-[436px] md:min-h-[744px] lg:min-h-[832px]'>
+			<CardList cardType='joined' pageKey='joined' />
+		</div>
+	);
 }
 
 export default MyPage;
