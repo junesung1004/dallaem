9c86b81fd61d6d9de8ca11e2510afe4003371d17
Chae <pce2416@gmail.com>
2025-03-04 03:03:00 +0900

    refactor: page 하위 컴포넌트 추가

---
+++ src/app/(home)/all-reviews/ReviewsPage.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import FilterList from '@/components/Filtering/FIlterList';
+import PageNavbar from '@/components/PageNav/PageNavbar';
+import PageInfo from '@/components/PageInfo/PageInfo';
+import ReviewCardList from './_components/ReviewCardList';
+import ReviewSummary from './_components/ReviewSummary';
+import FilterProvider from '@/context/FilterContent';
+
+export default function ReviewsPage() {
+	return (
+		<FilterProvider>
+			<div className='flex flex-col gap-3 pb-4 border-b-2 border-gray-200 '>
+				<PageInfo pageKey='reviews' />
+				<PageNavbar pageKey='meetings' />
+			</div>
+			<div className='flex flex-col gap-4 pt-5 pb-2 sticky top-0 z-10 bg-gray-50'>
+				<ReviewSummary />
+				<FilterList enabledFilters={['location', 'date', 'sortByReview']} />
+			</div>
+			<div className='flex flex-col gap-4 p-4'>
+				<ReviewCardList />
+			</div>
+		</FilterProvider>
+	);
+}
