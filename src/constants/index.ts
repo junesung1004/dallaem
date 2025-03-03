0e47f6dc65b63c304852df8b3ffd9af9f7f1b1ab
Chae <pce2416@gmail.com>
2025-03-03 12:55:23 +0900

    chore: pageInfo 이미지 변경
    깨졌던 이미지 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,17 +11,17 @@ export const PAGE_INFO: Record<string, PageInfoData> = {
 	meetings: {
 		title: '함께 할 사람이 없나요?',
 		description: '지금 모임에 참여해보세요',
-		src: '/images/head/headClass.png',
+		src: '/images/head/headClass.svg',
 	},
 	saved: {
 		title: '찜한 모임',
 		description: '지금 모임에 참여해보세요',
-		src: '/images/head/headSaved.png',
+		src: '/images/head/headSaved.svg',
 	},
 	reviews: {
 		title: '모든 리뷰',
 		description: '같이달램을 이용한 분들은 이렇게 느꼈어요🫶',
-		src: '/images/head/headReview.png',
+		src: '/images/head/headReview.svg',
 	},
 };
 
