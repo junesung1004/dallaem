29a1fecbd40700b24dfa89b7d3ac00ba0c2975b9
avocad06 <miggo2704@gmail.com>
2025-03-07 16:11:28 +0900

    feat: 마이페이지 SSR 전환
    나의 모임, 작성가능한 리뷰, 내가 만든 모임 페이지 작업

--- src/app/(home)/mypage/createdMeetings/page.tsx
+++ src/app/(home)/mypage/createdMeetings/page.tsx
@@ -1,9 +1,37 @@
+import { cookies } from 'next/headers';
 import CardList from '../components/CardList/CardList';
+import { BASE_URL } from '@/constants';
+import type { MyMeeting } from '@/types/meetingsType';
+import { IUser } from '@/types/userType';
 
-function Page() {
+async function Page() {
+	const cookieStore = await cookies();
+	const token = cookieStore.get('token');
+
+	const userInfoRes = await fetch(`${BASE_URL}/auths/user`, {
+		headers: {
+			Authorization: `Bearer ${token?.value}`,
+		},
+	});
+
+	if (!userInfoRes?.ok) return new Error('invalid token');
+	const userInfo: IUser = await userInfoRes.json();
+	const userId = userInfo.id;
+
+	const res = await fetch(`${BASE_URL}/gatherings/?createdBy=${userId}`, {
+		method: 'GET',
+		cache: 'no-store',
+	});
+
+	const initialMyMeetings: MyMeeting[] = await res?.json();
+
 	return (
 		<div className='flex min-h-[436px] md:min-h-[744px] lg:min-h-[673px]'>
-			<CardList cardType='hosted' pageKey='hosted' />
+			<CardList
+				cardType='hosted'
+				pageKey='hosted'
+				initialData={initialMyMeetings}
+			/>
 		</div>
 	);
 }
