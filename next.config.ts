7e50d0c752390b789dd07eaadd60440bba457959
avocad06 <miggo2704@gmail.com>
2025-03-11 06:03:22 +0900

    test: webpack config 추가해서 vercel var는 잘받는지 확인

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ module.exports = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	//@ts-ignore
+	webpack: (config, { isServer }) => {
+		// 서버 사이드에서만 환경 변수를 확인하려면
+		if (isServer) {
+			console.log('BASE_URL on build: ', process.env.BASE_URL);
+		}
+		return config;
+	},
 	reactStrictMode: true,
 	images: {
 		// domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
