bc342e3fa162c881379129400ad95c9f19b8b00b
심우석 <dntjr5741@naver.com>
2025-03-10 18:35:23 +0900

    fix: page type 오류

--- src/app/(home)/meeting/[id]/page.tsx
+++ src/app/(home)/meeting/[id]/page.tsx
@@ -1,13 +1,11 @@
 import DetailPage from './DetailPage';
 import ReviewPage from './ReviewPage';
+
 import { getDetailMeetingData } from '@/api/detail-meeting/getDetailMeetingDate';
 
-export default async function MeetingPage({
-	params,
-}: {
-	params: { id: string };
-}) {
-	const { id } = params;
+type Params = Promise<{ id: string }>;
+export default async function Page({ params }: { params: Params }) {
+	const { id } = await params;
 
 	const detailData = await getDetailMeetingData(id);
 
