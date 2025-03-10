7c1d172982c0d8feda439d232eec1dc04eebe34e
심우석 <dntjr5741@naver.com>
2025-03-10 14:36:04 +0900

    refactor: 페이지네이션, 리뷰데이터 분리

--- src/app/(home)/meeting/[id]/ReviewClient.tsx
+++ src/app/(home)/meeting/[id]/ReviewClient.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Pagination } from '../_components/Pagination';
 import { useParams } from 'next/navigation';
 import useDetailReviewData from '@/hooks/query/useDetailReviewData';
+import ReviewCard from '@/components/ReviewCard/ReviewCard';
 
 export default function ReviewClient() {
 	const [currentPage, setCurrentPage] = useState(1);
@@ -18,20 +19,21 @@ export default function ReviewClient() {
 		error: reviewError,
 	} = useDetailReviewData({ gatheringId: id, limit, currentPage });
 
+	const reviews = reviewData?.data || [];
+	const totalPages = Math.ceil((reviewData?.totalItemCount || 0) / limit);
+
 	const handlePageChange = (page: number) => {
-		if (page >= 1) {
+		if (page >= 1 && page <= totalPages) {
 			setCurrentPage(page);
 		}
 	};
-
-	const reviews = reviewData?.data || [];
-	const totalPages = Math.ceil((reviewData?.totalItemCount || 0) / limit);
 
 	return (
 		<div className='border-t-2 border-gray-300 mt-10 p-5 h-screen bg-white'>
 			<div className='mb-4 font-bold text-lg'>
 				이용자들은 이 프로그램을 이렇게 느꼈어요!!
 			</div>
+
 			{isReviewLoading ? (
 				<div className='text-center text-gray-500 mt-60'>
 					리뷰를 불러오는 중...
@@ -46,12 +48,30 @@ export default function ReviewClient() {
 					아직 작성된 리뷰가 없어요
 				</div>
 			) : (
-				<Pagination
-					currentData={reviews}
-					currentPage={currentPage}
-					totalPages={totalPages}
-					onPageChange={handlePageChange}
-				/>
+				<>
+					<ul>
+						{reviews.map((review) => (
+							<ReviewCard key={review.id} isDetailPage={true}>
+								<ReviewCard.ImageSection />
+								<ReviewCard.ReviewLayout isDetailPage={true}>
+									<ReviewCard.HeartScore score={review.score} />
+									<ReviewCard.Content comment={review.comment} />
+									<ReviewCard.EtcInfo
+										userIcon={review.Gathering.image}
+										nickname={review.User.name}
+										date={review.Gathering.dateTime}
+									/>
+								</ReviewCard.ReviewLayout>
+							</ReviewCard>
+						))}
+					</ul>
+
+					<Pagination
+						currentPage={currentPage}
+						totalPages={totalPages}
+						onPageChange={handlePageChange}
+					/>
+				</>
 			)}
 		</div>
 	);
