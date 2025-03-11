020c6ec715bae67c152ead7615c3c0d379077af6
junesung1004 <junecoding1004@gmail.com>
2025-03-11 11:38:47 +0900

    feat: 테스트 코드 초기 설정 및 기본작업 추가

---
+++ src/app/(home)/_components/HomeButton.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import HomeButton from './HomeButton';
+import '@testing-library/jest-dom';
+import { useRouter } from 'next/navigation';
+import { useGlobalModal } from '@/hooks/customs/useGlobalModal';
+import { useAuthStore } from '@/store/useAuthStore';
+
+//HomeButton 컴포넌트 안의 hook 관련된 mock 처리
+jest.mock('next/navigation', () => ({
+	useRouter: jest.fn(),
+}));
+
+jest.mock('@/store/useAuthStore', () => ({
+	useAuthStore: jest.fn(),
+}));
+
+jest.mock('@/hooks/customs/useGlobalModal', () => ({
+	useGlobalModal: jest.fn(() => ({
+		openModal: jest.fn(),
+		closeModal: jest.fn(),
+	})),
+}));
+
+describe('홈 모임 만들기 버튼 컴포넌트', () => {
+	it('홈 안에 버튼 dom이 잘 렌더링 되는지 test code', () => {
+		render(<HomeButton />);
+
+		//버튼 컴포넌트가 잘 렌더링 되는지 확인
+		const button = screen.getByText('모임 만들기');
+		expect(button).toBeInTheDocument();
+	});
+});
