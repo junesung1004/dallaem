3c0c885962e105b18fb109cab69f2112ed34d21f
Chae <pce2416@gmail.com>
2025-03-17 13:08:17 +0900

    feat: 헤더 로고 변경

--- src/components/GNB/Header.tsx
+++ src/components/GNB/Header.tsx
@@ -7,6 +7,7 @@ import { usePathname } from 'next/navigation'; // 현재 경로 가져오기
 import { ProfileTooltip } from '@/components/GNB/ProfileTooltip';
 import { useLikeNotify } from '@/hooks/customs/useLikeNotify';
 import Badge from '../Badge/Badge';
+import Image from 'next/image';
 
 export default function Header() {
 	const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
@@ -15,12 +16,19 @@ export default function Header() {
 
 	return (
 		<header className='flex items-center justify-center w-full h-[56px] md:h-[60px] bg-gradient-to-r from-primary-600 to-secondary-600 border-black border-b-2 px-4 md:px-6 lg:px-[106px]'>
-			<div className='max-w-[1200px] w-full'>
-				<nav className='md:mx-auto w-full flex justify-between items-center text-xs md:text-base lg:text-lg text-white font-semibold'>
-					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
-						<li className='font-extrabold'>
-							<Link href={'/'}>마음 달램</Link>
-						</li>
+			<div className='max-w-[1200px] w-full flex items-center justify-between'>
+				<div className='mr-2'>
+					<Link href={'/'}>
+						<Image
+							src='/images/logo/logo.png'
+							alt='logo'
+							width={100}
+							height={48}
+						/>
+					</Link>
+				</div>
+				<nav className='flex-1 flex'>
+					<ul className='flex gap-4 md:gap-4 lg:gap-6 text-xs md:text-base lg:text-lg text-white font-semibold'>
 						<li>
 							<Link
 								href={'/'}
@@ -29,7 +37,7 @@ export default function Header() {
 								모임 찾기
 							</Link>
 						</li>
-						<li className='flex items-center gap-[5px]'>
+						<li className='flex items-center gap-2'>
 							<Link
 								href={'/favorite-meetings'}
 								className={
@@ -51,22 +59,19 @@ export default function Header() {
 							</Link>
 						</li>
 					</ul>
-
-					<ul className='flex gap-2 sm:gap-4 lg:gap-5'>
-						<li>
-							{isLoggedIn ? (
-								<ProfileTooltip />
-							) : (
-								<Link
-									href='/login'
-									className={pathname === '/login' ? 'text-gray-900' : ''}
-								>
-									로그인
-								</Link>
-							)}
-						</li>
-					</ul>
 				</nav>
+				<div className='flex-shrink-0'>
+					{isLoggedIn ? (
+						<ProfileTooltip />
+					) : (
+						<Link
+							href='/login'
+							className={pathname === '/login' ? 'text-gray-900' : ''}
+						>
+							로그인
+						</Link>
+					)}
+				</div>
 			</div>
 		</header>
 	);
